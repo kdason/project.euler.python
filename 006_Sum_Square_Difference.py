298930bd8ea32677528707daa8c4b6424100c480
Ken Dason <kdason@gmail.com>
2018-05-08 16:24:00 -0400

    Initial Commit

--- 006_Sum_Square_Difference.py
+++ 006_Sum_Square_Difference.py
@@ -9,3 +9,11 @@ Hence the difference between the sum of the squares of the first ten natural num
 
 Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 """
+
+
+limit = 100
+sum_of_squares = sum([x**2 for x in range(limit + 1)])
+square_of_sum = sum(range(limit + 1))**2
+
+ans = 'The difference between the sum of the squares of the first one hundred natural numbers and the square of the sum is {} - {} = {}.'
+print(ans.format(square_of_sum, sum_of_squares, square_of_sum - sum_of_squares))
